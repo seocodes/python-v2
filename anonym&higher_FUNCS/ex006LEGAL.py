f99add1306943bfe201beba2ca93ecb86be24a89
seo.dev <azanettev@gmail.com>
2024-09-10 16:37:05 -0300

    correcao ex 6

--- anonym&higher_FUNCS/ex006LEGAL.py
+++ anonym&higher_FUNCS/ex006LEGAL.py
@@ -2,3 +2,5 @@ names = ['augusto', 'mariane', 'alice', 'pedro', 'jow']
 
 nome_ordem = sorted(names, key=lambda x: len(x))   #quando usa key o parametro é na frente
 print(nome_ordem)               #key define o que vai ordenar a lista
+
+#dava pra fazer key=len 
