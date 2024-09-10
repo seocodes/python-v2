9749d6ce290fdbae23f0108daf8f7bc7dc9a0901
seo.dev <azanettev@gmail.com>
2024-09-10 16:24:34 -0300

    corrigindo a 13, revisar, é dificil

--- anonym&higher_FUNCS/ex013DFICILPKRL.py
+++ anonym&higher_FUNCS/ex013DFICILPKRL.py
@@ -11,8 +11,11 @@ def salariosFuncionarios():
 dict1 = salariosFuncionarios()
 porc_aumento = int(input('Digite a porcentagem do aumento: '))
 
-salarioMaiorQ3000 = dict(filter(lambda x: x[1] > 3000, dict1.items()))
+salarioMaiorQ3000 = dict(filter(lambda x: x[1] > 3000, dict1.items()))  #Sim, isso retorna um dicionário com todos os nomes
+#só retorna os dicionários que tem > 3000 (ainda em forma de dicionário pq usei o dict)
+
 aumento = dict(map(lambda x: (x[0], x[1] + (x[1] * (porc_aumento / 100))), salarioMaiorQ3000.items()))
+#a "," depois do x[0] é pra separar a chave do valor, não usar ":" em dict lambdas
 
 for nome, novoSalario in aumento.items():
     dict1[nome] = novoSalario
