902c17a6a768d01ba756299cf989528c77ba4e88
seo.dev <azanettev@gmail.com>
2024-09-10 16:58:40 -0300

    add coisas no ex7

--- anonym&higher_FUNCS/ex011.py
+++ anonym&higher_FUNCS/ex011.py
@@ -1,6 +1,7 @@
 palavras = ['augusto', 'maçã', 'ogro', 'melancia', 'a', 'b', 'c']
-palavras_maisq3 = list(filter(lambda x: len(x) >= 3, palavras))
+palavras_maisq3 = list(filter(lambda x: len(x) > 3, palavras))
+palavras_len =  list(map(lambda x: len(x) , palavras_maisq3))
 
 for palavra in palavras_maisq3:
-    print(len(palavra))
-
+    print(palavra)
+print(palavras_len)
