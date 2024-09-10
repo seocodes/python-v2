e17c1f203f9acb44863705b6f71486d979c65507
seo.dev <azanettev@gmail.com>
2024-09-10 16:38:44 -0300

    a

--- anonym&higher_FUNCS/ex011.py
+++ anonym&higher_FUNCS/ex011.py
@@ -1,7 +1,6 @@
 palavras = ['augusto', 'maçã', 'ogro', 'melancia', 'a', 'b', 'c']
 palavras_maisq3 = list(filter(lambda x: len(x) >= 3, palavras))
 
-print(palavras_maisq3)
 for palavra in palavras_maisq3:
     print(len(palavra))
 
